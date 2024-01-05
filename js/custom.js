6093cf14e71185bca84ee1ead57281b540cdcd0e
Mawiz Shahab <97441655+MawizShahab@users.noreply.github.com>
2024-01-05 17:32:51 +0500

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -77,7 +77,7 @@ $(document).ready(function () {
 // Js to handle starter modal and more selections toggle for AI
 
 $(document).ready(function () {
-  // $("#starterModal").modal("show");
+  $("#starterModal").modal("show");
   $(
     "#starterModal #moreOptionsLink, #generateNewIntroModal #moreOptionsLink, #generateNewTagLineModal #moreOptionsLink, #generateNewTagModal #moreOptionsLink, #generateNewDescriptionModal #moreOptionsLink, #regenerateOfferingModal #moreOptionsLink, #generateTitleModal #moreOptionsLink, #generateOfferingDescriptionModal #moreOptionsLink, #generateOfferingMoreInfoModal #moreOptionsLink, #generateNewValuesModal #moreOptionsLink, #generateValuesDescriptionModal #moreOptionsLink, #generateValuesTitleModal #moreOptionsLink, #generateNewBackgroundModal #moreOptionsLink, #generateBackgroundTitleModal #moreOptionsLink, #generateBackgroundDescriptionModal #moreOptionsLink, #generateAddEditScopeTitleModal #moreOptionsLink, #generateAddEditScopeDescriptionModal #moreOptionsLink, #generateNewResourceDescriptionModal #moreOptionsLink, #generateResourceTitleModal #moreOptionsLink, #generateResourceDescriptionModal #moreOptionsLink"
   ).click(function () {
@@ -97,4 +97,21 @@ $(document).ready(function () {
       ".upload-profile-container"
     );
   }
+});
+
+/* FAQ Toggle */
+
+$(document).ready(function () {
+  $(".question").click(function () {
+    const answer = $(this).next(".answer");
+    const icon = $(this).find(".toggle-icon");
+
+    if (answer.is(":visible")) {
+      answer.slideUp();
+      icon.text("+");
+    } else {
+      answer.slideDown();
+      icon.text("X");
+    }
+  });
 });
